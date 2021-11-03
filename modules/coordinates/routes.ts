1ecb96da3d9f6013e9689ee8dec168cb90a26287
Rosmee.Dev <prateep.sukthavorn@gmail.com>
2021-11-04 02:57:26 +0700

    config

---
+++ modules/coordinates/routes.ts
@@ -0,0 +1,11 @@
+// src/modules/coordinates/routes.ts
+import { Repository } from "typeorm";
+import { Coordinates } from "./entity"
+
+export default async (server, options, next) => {
+  server.get("",{
+    
+  });
+
+  next()
+}
--- modules/vehicles/routes.ts
+++ modules/vehicles/routes.ts
@@ -1,7 +1,11 @@
 // src/modules/vehicles/routes.ts
 import { Repository } from "typeorm";
+import { Vehicles } from "./entity"
 
 export default async (server, options, next) => {
+  server.get("",{
     
-    next()
-  }
+  });
+
+  next()
+}
